69ac9642da2fb0d31cdd3abe71b158c03d96e229
HernanCagiao <hernan.cageao@gmail.com>
2022-08-07 12:58:10 -0300

    segundo cambio 07 agosto

--- src/Components/IconsQuality/IconsQuality.js
+++ src/Components/IconsQuality/IconsQuality.js
@@ -7,7 +7,7 @@ const IconsQuality=() => {
         <div className="justify-center pt-20 text-5xl font-bold text-center text-yellow-300 bg-slate-800 font-Poppins">
             <h2><span className="text-white">¿Por qué</span> elegir nuestros productos?</h2>
         </div>
-        <div className="hidden sm:hidden sm:justify-evenly md:hidden md:justify-evenly lg:flex bg-slate-800 justify-evenly lg:mt-20">
+        <div className="hidden sm:hidden sm:justify-evenly md:hidden md:justify-arround lg:flex bg-slate-800 lg:justify-around 2xl:justify-around lg:mt-20">
             <img src="../images/telescope.png" className="h-24 w-30"  alt="Telescope"></img>
             <img src="../images/casa.png" className="h-24 w-30"  alt="House"></img>
             <img src="../images/manos.png" className="h-24 w-30"  alt="Hands"></img>
--- src/Components/Quality/index.jsx
+++ src/Components/Quality/index.jsx
@@ -7,7 +7,7 @@ const Quality = (props) =>  {
   <>
   
  
-      <div className='flex flex-wrap flex-grow font-bold h-100 bg-slate-800 justify-evenly px-auto '>     
+      <div className='flex flex-wrap flex-grow font-bold h-100 bg-slate-800 justify-evenly px-auto md:justify-around lg:justify-around xl:justify-around 2xl:justify-around'>     
       {props.contenido.map(atributo=> (
         <Card
            title={atributo.title}

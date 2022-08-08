f5b3310e0803af31234ef8a9d23b956df2d49638
BlondaKill <ma.lauramorales@gmail.com>
2022-08-08 16:58:11 -0300

    august8

--- src/Components/TellAFriend/index.jsx
+++ src/Components/TellAFriend/index.jsx
@@ -4,12 +4,12 @@ const TellAFriend = () => {
         <div className="flex flex-col items-center my-10 bg-black/75 bg-blend-overlay bg-tellafriend-banner">
             <h1 className="container mx-auto mt-14 mb-10 text-4xl font-bold text-center text-yellow-300 strong font-['Cormorant']"> Dile a Tus Amigos </h1> 
             <div className="flex flex-row justify-center mb-14">          
-                <img src= "/images/facebook.svg" alt=" " className="w-12 h-12 mx-3 bg-yellow-300"/>
-                <img src= "/images/instagram.svg" alt=" "className="w-12 h-12 mx-3 bg-yellow-300"/>
-                <img src= "/images/whatsapp.svg" alt=" "className="w-12 h-12 mx-3 bg-yellow-300"/>
-                <img src= "/images/messenger.svg" alt=" "className="w-12 h-12 mx-3 bg-yellow-300"/>
-                <img src= "/images/twitter.svg" alt=" "className="w-12 h-12 mx-3 bg-yellow-300"/>
-                <img src= "/images/telegram.svg" alt=" "className="w-12 h-12 mx-3 bg-yellow-300"/>    
+                <img src="/images/face.png" alt=" " className="w-12 h-12 mx-3 min-height" />
+                <img src= "/images/Instagram1.png" alt=" "className="w-12 h-12 mx-3 min-height"/>
+                <img src= "/images/Whatsapp.png" alt=" "className="w-12 h-12 mx-3 min-height"/>
+                <img src= "/images/messenger.png" alt=" "className="w-12 h-12 mx-3 min-height"/>
+                <img src= "/images/twitter.png" alt=" "className="w-12 h-12 mx-3 min-height"/>
+                <img src= "/images/teleg.png" alt=" "className="w-12 h-12 mx-3 min-height"/>    
             </div>
         </div>
     )
